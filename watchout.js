193ba3c14b3acfdd702047ad3af0b2307dc4fce2
realmike33 <realmike33@gmail.com>
2014-12-20 16:24:58 -0800

    Ye

--- watchout.js
+++ watchout.js
@@ -1,6 +1,7 @@
 var gameBoard = d3.select('body').append('svg');
 var counter = 0;
 var high = 0;
+
 var storedEnemy = [];
 
 d3.select('button').on('click', function(){location.reload();})
@@ -16,23 +17,27 @@ var highScore = function(){
   }
 }
 
-var dragmove = function(d, i){
+var colorChange = function(){
+  var random1 = Math.floor(Math.random() * 255);
+  var random2 = Math.floor(Math.random() * 255);
+  var random3 = Math.floor(Math.random() * 255);
+  d3.select('circle')
+  .transition()
+  .duration(300)
+  .style("fill", function(){return d3.rgb(random1, random2, random3)})
+}
+
+setInterval(colorChange, 300);
+
+var dragmove = function(){
   counter += 1;
   updateScore();
   highScore();
-  var x = d3.event.x;
-  var y = d3.event.y;
+  var x = d3.event.x - user.attr('r');
+  var y = d3.event.y - user.attr('r');
   d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
-  d3.selectAll('rect').data(storedEnemy).attr('x', function(item){
-    if(this.item === x){
-      console.log('collision');
-    }
-  }).attr('y', function(item){
-    if(this.item === y){
-      console.log('collision');
-    }
-  })
 }
+
 
 var drag = d3.behavior.drag().on('drag', dragmove);
 
@@ -42,12 +47,12 @@ var user = gameBoard.append('circle')
 .attr('cy', 15)
 .attr('r', 15)
 .style('fill', 'white')
-.call(drag);
+.call(drag)
 
 
 
 var enemy = function(xTime, yTime){
-var enemyNum = 20;
+  var enemyNum = 20;
   var innerFunc = function(xTime, yTime){
     if(enemyNum === 0){
       return;
@@ -66,14 +71,62 @@ var enemyNum = 20;
 };
 enemy();
 
-var moveEnemies = function(){
-  gameBoard.selectAll('rect')
+var moveEnemies = function(element){
+  element
   .transition()
   .duration(3000)
   .attr('x', function(d){ return Math.floor(Math.random() * 700)})
   .attr('y', function(d){ return Math.floor(Math.random() * 700)})
-  .style('fill', function(d){ return 'green'})
+  .style('fill', 'red')
+  .delay(00)
+  .transition()
+  .duration(1000)
+  .style('fill', 'green')
+  .each('end', function(){
+    moveEnemies(d3.select(this))
+  })
 }
-moveEnemies();
-setInterval(moveEnemies, 4000);
+moveEnemies(gameBoard.selectAll('rect'));
+
+var detectCollision = function(){
+  var collision = false;
+
+  gameBoard.selectAll('rect').each(function(){
+    console.log(this.offsetLeft)
+    var cx = this.offsetLeft + gameBoard.attr('x');
+    var cy = this.offsetTop + gameBoard.attr('y')
+    var x = cx - user.attr('cx')
+    var y = cy - user.attr('cy')
+    if(Math.sqrt(x*x + y*y) < user.attr('r')*2){
+      collision = true;
+    }
+  });
+
+  if(collision){
+    console.log('collide!')
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
